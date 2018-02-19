5e7eeb1996790b23c1f8272c9b5c32984398e7f4
Herry <her0satr@gmail.com>
2018-02-19 08:55:12 +0700

    Add request post

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public http: Http) {
   }
 
+  ionViewDidLoad() {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    let body = {
+      message: 'Do you hear me  ?'
+    }
+
+    this.http.post('http://localhost:3000/api/test', JSON.stringify(body), { headers: headers })
+      .map( res => res.json() )
+      .subscribe( data => {
+        console.log(data);
+      });
+  }
 }
